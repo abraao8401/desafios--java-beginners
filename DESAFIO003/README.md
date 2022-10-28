229a162de0ae90064d5d4e22638879b48bb89b5b
Abraao Santiago Moreira <santiagoabraao17@gmail.com>
2022-10-28 15:20:04 -0300

    5commit

---
+++ DESAFIO003/README.md
@@ -0,0 +1,12 @@
+
+## Descrição
+
+Neste desafio, receba um número inteiro N, calcule e imprima o somatório de todos os números de N até 0.   
+
+## Entrada
+
+A Entrada será composta por um número inteiro, N. 
+
+## Saída
+
+Será  impresso o somatório de N até 0, como no exemplo a baixo:
---
+++ DESAFIO003/src/ChamadaRecursiva.java
@@ -0,0 +1,27 @@
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.  
+
+import java.util.Scanner;
+
+public class ChamadaRecursiva {
+
+    public static void main(String[] args) {
+
+        try (Scanner numero = new Scanner(System.in)) {
+            int  n = numero.nextInt();
+            System.out.println(somatorio(n));
+        }
+    }
+
+    /**
+     * Método útil que calcula o somatório de um número usando recursividade.
+     */
+    static int somatorio(int numero) {
+        if (numero == 0) {
+            return 0;
+        } else {
+            return numero + somatorio(numero - 1);
+        }
+    }
+}
